9a3818dc42eb060c0900278cb455736585bb11c2
Itsuki Aoyagi <mail@yagi2.dev>
2021-06-11 21:58:53 +0900

    initial commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Itsuki Aoyagi on 2021/06/11.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
